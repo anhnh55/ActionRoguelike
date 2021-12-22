1bc4239aee1deb3f66418bc4aa087dad74d8437e
anhnh55 <nha.2731991@gmail.com>
2021-12-22 21:24:47 +0900

    Shoot projectile toward crosshair.

--- Source/ActionRoguelike/SCharacter.cpp
+++ Source/ActionRoguelike/SCharacter.cpp
@@ -29,10 +29,47 @@ ASCharacter::ASCharacter()
 void ASCharacter::PrimaryAttack_Timelapsed()
 {
 	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
-	FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
+	
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 	SpawnParams.Instigator = this;
+
+	//cast line from camera
+	
+	//Viewport Size
+	const FVector2D ViewportSize = FVector2D(GEngine->GameViewport->Viewport->GetSizeXY());
+
+	//Viewport Center!            
+	const FVector2D  ViewportCenter = FVector2D(ViewportSize.X / 2, ViewportSize.Y / 2);
+	FVector ReticleWorldLocation;
+	FVector ReticleWorldDirection;
+	GetWorld()->GetFirstPlayerController()->DeprojectScreenPositionToWorld(ViewportCenter.X, ViewportCenter.Y, ReticleWorldLocation, ReticleWorldDirection);
+
+	UE_LOG(LogTemp, Warning, TEXT("CenterX %f, CenterY %f,  Loc %s, Dir %s"), ViewportCenter.X, ViewportCenter.Y, *ReticleWorldLocation.ToString(), *ReticleWorldDirection.ToString());
+	//FVector TraceStart = CameraComp->GetComponentLocation();
+	
+	//FVector TraceEnd = CameraComp->GetComponentLocation() + CameraComp->GetComponentRotation().Vector() * 5000.0f;
+	FVector TraceStart = ReticleWorldLocation;
+	FVector TraceEnd = ReticleWorldLocation + ReticleWorldDirection*1000.0f;
+	
+	FHitResult Hit;
+	FCollisionObjectQueryParams ObjectQueryParams;
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_Pawn);
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	//GetActorEyesViewPoint(CameraLocation, CameraRotation);
+	if(GetWorld()->LineTraceSingleByObjectType(Hit,TraceStart, TraceEnd, ObjectQueryParams, QueryParams))
+	{
+		TraceEnd = Hit.ImpactPoint;
+	}
+	
+
+	FRotator ProjRotation = FRotationMatrix::MakeFromX(TraceEnd - HandLocation).Rotator();
+	FTransform SpawnTM = FTransform(ProjRotation, HandLocation);
 
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
@@ -89,13 +126,13 @@ void ASCharacter::Tick(float DeltaTime)
 	// Offset to the right of pawn
 	LineStart += GetActorRightVector() * 100.0f;
 	// Set line end in direction of the actor's forward
-	FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
-	// Draw Actor's Direction
-	DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+	//FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
+	//// Draw Actor's Direction
+	//DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
 
-	FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
-	// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
-	DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
+	//FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
+	//// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
+	//DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
 }
 
 // Called to bind functionality to input
