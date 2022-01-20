1e440a200b478592aaf07c111fcd56a336c8a6f5
anhnh55 <nha.2731991@gmail.com>
2022-01-20 22:56:50 +0900

    Add Environment Query to make AI move more dynamically

--- Source/ActionRoguelike/Private/AI/SAICharacter.cpp
+++ Source/ActionRoguelike/Private/AI/SAICharacter.cpp
@@ -3,12 +3,16 @@
 
 #include "AI/SAICharacter.h"
 
+#include "AIController.h"
+#include "DrawDebugHelpers.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 // Sets default values
 ASAICharacter::ASAICharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>("PawnSensingComp");
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +20,23 @@ void ASAICharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ASAICharacter::OnPawnSeen(APawn* Pawn)
+{
+	AAIController* AIC = Cast<AAIController>(GetController());
+	if(AIC)
+	{
+		UBlackboardComponent* BBComp = AIC->GetBlackboardComponent();
+		BBComp->SetValueAsObject("TargetActor", Pawn);
+		//DrawDebugString(GetWorld(), GetActorLocation(), "PLAYER SPOTTED", nullptr, FColor::White, 4.0f, true);
+	}
+}
+
+void ASAICharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &ASAICharacter::OnPawnSeen);
 }
 
 // Called every frame
--- Source/ActionRoguelike/Public/AI/SAICharacter.h
+++ Source/ActionRoguelike/Public/AI/SAICharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Perception/PawnSensingComponent.h"
 #include "SAICharacter.generated.h"
 
 UCLASS()
@@ -19,6 +20,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category="Component")
+	UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+		void OnPawnSeen(APawn* Pawn);
+
+	virtual void PostInitializeComponents() override;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
